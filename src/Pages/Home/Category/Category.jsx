402677b3928efda6c6d8b9d9c4b2142f60519bc0
sayed132 <rafsansayed132@gmail.com>
2023-09-26 19:26:01 +0600

    category

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
 import { RiStarHalfSFill, RiStarSFill } from "react-icons/ri";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const Category = () => {
     const [categories, setCategories] = useState([]);
@@ -8,68 +12,69 @@ const Category = () => {
         fetch('category.json')
             .then(res => res.json())
             .then(data => setCategories(data))
-    }, [])
+    }, []);
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4, // Number of slides to show at a time
+        slidesToScroll: 1, // Number of slides to scroll
+        autoplay: true, // Autoplay enabled
+        autoplaySpeed: 3000, // Autoplay every 1 second (1000ms
+    };
     return (
-        <div>
-            <div>
-                {/* {
-                    categories.map(category => <Link className='text-white' to={`/category/${category.id}`} key={category.id}>
-                        <p className='bg-indigo-500 hover:bg-indigo-600 my-4 p-4 rounded-md'>
-                            {category.name}
-                        </p>
-                    </Link>)
-                } */}
-                <div className="grid grid-cols-12 gap-4 mx-auto items-center my-8">
-                    {
-                        categories.map(category =>
-                            <div className="col-span-3 h-24 border" key={category.id}>
-                                <div className="grid grid-cols-5 items-center mx-auto">
-                                    <div className="col-span-2 h-24 ">
-                                        <img className="h-24 w-28" src={category?.author?.img} alt="" />
+        <div className="my-6">
+            <Slider {...settings}>
+                {categories.map(category => (
+                    <div className=" px-2 " key={category.id}>
+                        {/* Your card content here */}
+                        <div className="grid grid-cols-4 shadow-md bg-gray-200  ">
+                            <div className="col-span-2 my-auto">
+                                <img className="h-28" src={category?.author?.img} alt="" />
+                            </div>
+
+                            {/* book details  */}
+                            <div className="col-span-full lg:col-span-2 ms-1">
+                                <div className="grid grid-cols-3 items-center mx-auto">
+                                    <div className=" form-control  col-span-full text-start ">
+                                        <div className="flex items-center gap-2">
+
+                                            <p className="font-bold text-gray-600">{category?.name}</p>
+
+                                        </div>
                                     </div>
+                                    <div className=" form-control  col-span-full text-start ">
+                                        <div className="flex items-center gap-2">
 
-                                    {/* book details  */}
-                                    <div className="col-span-full lg:col-span-3 ms-1">
-                                        <div className="grid grid-cols-3 items-center mx-auto">
-                                            <div className=" form-control  col-span-full text-start ">
-                                                <div className="flex items-center gap-2">
+                                            <p className="text-sm text-gray-500 font-medium">{category?.author?.name}</p>
+                                        </div>
+                                    </div>
+                                    <div className=" form-control  col-span-full text-start ">
+                                        <div className="flex items-center ">
+                                            <p className="flex text-warning text-xs">
+                                                <RiStarSFill></RiStarSFill> <RiStarSFill></RiStarSFill>
+                                                <RiStarSFill></RiStarSFill> <RiStarSFill></RiStarSFill>
+                                                <span className="text-warning "><RiStarHalfSFill></RiStarHalfSFill></span>
+                                            </p>
+                                            <p className="text-xs ms-1">{category?.rating?.total}</p>
 
-                                                    <p className="font-bold text-gray-600">{category?.name}</p>
+                                        </div>
+                                    </div>
+                                    <div className=" form-control  col-span-full text-start ">
+                                        <div className="flex items-center gap-2">
 
-                                                </div>
-                                            </div>
-                                            <div className=" form-control  col-span-full text-start ">
-                                                <div className="flex items-center gap-2">
+                                            <p className="font-semibold text-gray-600 ">TK.{category?.price}</p>
 
-                                                    <p className="text-sm text-gray-500 font-medium">{category?.author?.name }</p>
-                                                </div>
-                                            </div>
-                                            <div className=" form-control  col-span-full text-start ">
-                                                <div className="flex items-center ">
-                                                    <p className="flex text-warning text-xs">
-                                                        <RiStarSFill></RiStarSFill> <RiStarSFill></RiStarSFill>
-                                                        <RiStarSFill></RiStarSFill> <RiStarSFill></RiStarSFill>
-                                                        <span className="text-warning "><RiStarHalfSFill></RiStarHalfSFill></span>
-                                                    </p>
-                                                    <p className="text-xs ms-1">{ category?.rating?.total}</p>
-
-                                                </div>
-                                            </div>
-                                            <div className=" form-control  col-span-full text-start ">
-                                                <div className="flex items-center gap-2">
-
-                                                    <p className="font-semibold text-gray-600 ">TK.{ category?.price}</p>
-
-                                                </div>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>)
-                    }
-                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </Slider>
 
-            </div>
         </div>
     );
 };
